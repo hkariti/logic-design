5d2c1d77a4865b65d9f995f2d84d71a71a2125e3
Hagai Kariti <hkariti@gmail.com>
2016-05-19 20:19:27 +0300

    Use generate

--- wet3/Top.vhd
+++ wet3/Top.vhd
@@ -168,11 +168,14 @@ component Reg is
      );
 end component;
 
+type mux_outs is array(6 downto 0) of std_logic_vector(N-1 downto 0);
+type mux_selectors is array (6 downto 0) of std_logic_vector(1 downto 0);
+type reg_outs is array(4 downto 0) of std_logic_vector(N-1 downto 0);
 signal Ato2,Bto2,MinFrom2,MaxFrom2 : std_logic_vector (N-1 downto 0);
-signal S0, S1, S2, S3, S4, S5 : std_logic_vector(1 downto 0); -- Mux control
-signal WeA, WeB, WeC, WeD : std_logic; -- Register control
-signal m0_out, m1_out, m2_out, m3_out, m4_out, m5_out: std_logic_vector(N-1 downto 0);
-signal r0_out, r1_out, r2_out, r3_out : std_logic_vector(N-1 downto 0);
+signal mux_s : mux_selectors;
+signal reg_en: std_logic_vector(3 downto 0); 
+signal m_out : mux_outs;
+signal r_out : reg_outs;
 
 begin
 
@@ -180,36 +183,51 @@ U0 : Compare2 generic map (N=>N)
       port map (A=>Ato2,B=>Bto2,Max=>MaxFrom2,Min=>MinFrom2);
 
 U1 : ContComp
-      port map (CLK=>CLK,RSTn=>RSTn, WeA=>WeA, WeB=>WeB, WeC=>WeC, WeD=>WeD, s0=>s0, s1=>s1, s2=>s2, s3=>s3, s4=>s4, s5=>s5);
+      port map (CLK=>CLK,RSTn=>RSTn, WeA=>reg_en(0), WeB=>reg_en(1), WeC=>reg_en(2), WeD=>reg_en(3), s0=>mux_s(0), s1=>mux_s(1), s2=>mux_s(2), s3=>mux_s(3), s4=>mux_s(4), s5=>mux_s(5));
 
-r0: Reg
-      port map (CLK=>CLK, RSTn=>RSTn, D=>m0_out, Q=>r0_out, en=>WeA);
+gen_reg: for i in 0 to 3 generate
+    r: Reg
+        port map (CLK=>CLK, RSTn=>RSTn, d=>m_out(i), Q=>r_out(i), en=>reg_en(i));
+end generate;
 
-r1: Reg
-      port map (CLK=>CLK, RSTn=>RSTn, D=>m1_out, Q=>r1_out, en=>WeB);
+mux3_gen: for i in 0 to 3 generate
+    m3: Mux3 
+        port map (A => A, B => MinFrom2, C => MaxFrom2, Z => m_out(i), s=>mux_s(i));
+end generate;
 
-r2: Reg
-      port map (CLK=>CLK, RSTn=>RSTn, D=>m2_out, Q=>r2_out, en=>WeC);
+m4_0: Mux4
+    port map(A => r_out(0), B => r_out(1), C => r_out(2), D => r_out(3), Z => Ato2, s=>mux_s(4));
 
-r3: Reg
-      port map (CLK=>CLK, RSTn=>RSTn, D=>m3_out, Q=>r3_out, en=>WeD);
-
-m0: mux3
-    port map (A => A, B => MinFrom2, C => MaxFrom2, Z => m0_out, s=>s0);
-
-m1: mux3
-    port map (A => B, B => MinFrom2, C => MaxFrom2, Z => m1_out, s=>s1);
-
-m2: mux3
-    port map (A => C, B => MinFrom2, C => MaxFrom2, Z => m2_out, s=>s2);
-
-m3: mux3
-    port map (A => D, B => MinFrom2, C => MaxFrom2, Z => m3_out, s=>s3);
-
-m4: mux4
-    port map(A => r0_out, B => r1_out, C => r2_out, D => r3_out, Z => Ato2, s=>s4);
-
-m5: mux4
-    port map(A => r0_out, B => r1_out, C => r2_out, D => r3_out, Z => Bto2, s=>s5);
+m4_1: Mux4
+    port map(A => r_out(0), B => r_out(1), C => r_out(2), D => r_out(3), Z => Bto2, s=>mux_s(5));
+--r0: Reg
+--      port map (CLK=>CLK, RSTn=>RSTn, D=>m0_out, Q=>r0_out, en=>reg_en(0));
+--
+--r1: Reg
+--      port map (CLK=>CLK, RSTn=>RSTn, D=>m1_out, Q=>r1_out, en=>WeB);
+--
+--r2: Reg
+--      port map (CLK=>CLK, RSTn=>RSTn, D=>m2_out, Q=>r2_out, en=>WeC);
+--
+--r3: Reg
+--      port map (CLK=>CLK, RSTn=>RSTn, D=>m3_out, Q=>r3_out, en=>WeD);
+--
+--m0: mux3
+--    port map (A => A, B => MinFrom2, C => MaxFrom2, Z => m0_out, s=>s0);
+--
+--m1: mux3
+--    port map (A => B, B => MinFrom2, C => MaxFrom2, Z => m1_out, s=>s1);
+--
+--m2: mux3
+--    port map (A => C, B => MinFrom2, C => MaxFrom2, Z => m2_out, s=>s2);
+--
+--m3: mux3
+--    port map (A => D, B => MinFrom2, C => MaxFrom2, Z => m3_out, s=>s3);
+--
+--m4: mux4
+--    port map(A => r0_out, B => r1_out, C => r2_out, D => r3_out, Z => Ato2, s=>s4);
+--
+--m5: mux4
+--    port map(A => r0_out, B => r1_out, C => r2_out, D => r3_out, Z => Bto2, s=>s5);
 
 end architecture;
